e4e51132cced8ab78d12d702a863a88426ee562d
Carlitox-PC <Carlitox@Carlitox-PC>
2020-12-13 14:14:37 -0500

    Capa Logica

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+from models.clientes_models import clientesIn, clientesOut
+
+from models.compras_models import comprasIn, comprasOut
+
+import datetime
+from fastapi import FastAPI
+from fastapi import HTTPException
+api = FastAPI()
+ 
