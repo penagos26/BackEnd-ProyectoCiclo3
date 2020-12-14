8a6f0b7cca6a580396d94dd165456a100055f9f9
penagos26 <75628346+penagos26@users.noreply.github.com>
2020-12-13 22:34:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -38,10 +38,10 @@ async def Obtener_cliente(cc: int):
     if clientes_in_db != None:
         clientes_out = ClientesOut(**clientes_in_db.dict())
         #Mostrar los resultados de las compras
-        #compras_out = get_Compra_Cliente(cc)
+        #compras_out = 
         #Resultados = {"Cliente":clientes_out},{"Compras":get_Compra_Cliente(cc)}
         Resultados["Cliente"] = clientes_out
-        Resultados["Compras"] = compras_out
+        Resultados["Compras"] = get_Compra_Cliente(cc)
         return Resultados #clientes_out, compras_out 
         #raise HTTPException(status_code=200, detail="El cliente existe")
        
